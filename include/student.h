4b7b90cf041b57c13fa0465b150fc8818647cae3
omerbd21 <omerbendavid21@gmail.com>
2020-09-18 10:53:27 +0300

    added file structure and 3 base headers for classes

---
+++ include/student.h
@@ -0,0 +1,13 @@
+// student.h
+class Student
+{
+private:
+    int id;
+    char[15] name;
+    int grade;
+public:
+    int GetId();
+    char GetName();
+    int GetGrade();
+    void GradeApp();
+};
